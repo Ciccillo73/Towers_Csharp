a519804507975b20d97d908c67181217bfcc2949
Francesco <med.dama@gmail.com>
2020-07-21 21:09:00 +0100

    SetUp the TestDiningRoom class

--- Test/TestDiningRoom.cs
+++ Test/TestDiningRoom.cs
@@ -1,7 +1,22 @@
+using NUnit.Framework;
+
 namespace Towers_cSharp
 {
     public class TestDiningRoom
     {
+        private DiningRoom _diningRoom;
+        private Guest _guest1;
+        private Guest _guest2;
+        
+        
+        [SetUp]
+        public void SetUp()
+        {
+            _diningRoom = new DiningRoom("Heaven");
+            _guest1 = new Guest("Phil");
+            _guest2 = new Guest("MArk");
+        }
+        
         
     }
 }
