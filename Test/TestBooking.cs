7ce26ca0bf4e7948c33ef3329250a835e827475f
Francesco <med.dama@gmail.com>
2020-07-21 15:46:42 +0100

    Create TestBooking class

---
+++ Test/TestBooking.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+
+namespace Towers_cSharp
+{
+    public class TestBooking
+    {
+        
+        
+        [SetUp]
+        public void SetUp()
+        {
+            
+        }
+        
+        [Test]
+        public void test()
+        {
+            
+        }
+    }
+}
