52bf090855c61f25720ce49f1e47f002b1cbe6fd
Francesco <med.dama@gmail.com>
2020-07-21 20:59:15 +0100

    Add _name and _guests fields, create Constructor

--- DiningRoom.cs
+++ DiningRoom.cs
@@ -1,7 +1,25 @@
+using System.Collections.Generic;
+
 namespace Towers_cSharp
 {
     public class DiningRoom
     {
+        private string _name;
+        private List<Guest> _guests;
+
+        public DiningRoom(string name)
+        {
+            _name = name;
+        }
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+        
         
     }
+    
+    
 }
