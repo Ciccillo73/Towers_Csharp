4083ffbaac92daf2a9bb45cbe10f8aadd5819cc5
Francesco <med.dama@gmail.com>
2020-07-21 15:32:01 +0100

    Add Constructor and properties

--- Booking.cs
+++ Booking.cs
@@ -4,5 +4,24 @@ namespace Towers_cSharp
     {
         private int _numberOfNights;
         private Bedroom _bedroom;
+
+        public Booking(int numberOfNights, Bedroom bedroom)
+        {
+            _numberOfNights = numberOfNights;
+            _bedroom = bedroom;
+        }
+
+        public int NumberOfNights
+        {
+            get => _numberOfNights;
+            set => _numberOfNights = value;
+        }
+
+        public Bedroom Bedroom
+        {
+            get => _bedroom;
+            set => _bedroom = value;
+        }
     }
+    
 }
